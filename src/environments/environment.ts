e90ae9978faf743c9714d92b394635656aa74bad
jackdaniel1996 <Danielderklein@googlemail.com>
2026-08-07 10:09:13 +0200

    fix link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     debug: true,
     // apiEndpoint: 'http://localhost:3000',
-    apiEndpoint: 'https://farkle-api.danielkle.in/',
+    apiEndpoint: 'https://farkle-api.danielkle.in',
 };
