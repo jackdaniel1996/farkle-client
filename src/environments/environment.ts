6de0c4f0b8de949b5d8604a6b66da35311349bbe
jackdaniel1996 <Danielderklein@googlemail.com>
2026-08-14 16:06:53 +0200

    endpoint for main

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     debug: true,
-    apiEndpoint: 'http://localhost:3000',
-    // apiEndpoint: 'https://farkle-api.onrender.com',
-    // apiEndpoint: 'https://farkle-api.danielkle.in',
+    // apiEndpoint: 'http://localhost:3000',
+    apiEndpoint: 'https://farkle-api.onrender.com',
 };
