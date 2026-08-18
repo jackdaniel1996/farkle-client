29e082878d95be6d6d1c688143255db6b53413fa
jackdaniel1996 <Danielderklein@googlemail.com>
2026-08-18 16:10:45 +0200

    fixed and expanded hints

--- src/app/components/hint-container/hint-container.ts
+++ src/app/components/hint-container/hint-container.ts
@@ -1,5 +1,6 @@
 import { Component, computed, Input } from '@angular/core';
 import { GameState } from '../../shared/models';
+import { LobbyService } from '../../services/lobby.service';
 
 @Component({
   selector: 'app-hint-container',
@@ -8,11 +9,23 @@ import { GameState } from '../../shared/models';
   styleUrl: './hint-container.scss',
 })
 export class HintContainer {
-  @Input() game: GameState | undefined = undefined;
+  constructor(
+    private lobbyService: LobbyService,
+  ) {}
+
+  game = computed(() => {
+    return this.lobbyService.activeLobby()?.game;
+  });
 
   activePlayer = computed(() => {
-    const game = this.game;
+    const game = this.game();
     if (!game) return undefined;
     return game.players?.find(p => p.id == game.currentPlayerId);
   });
+
+  selectableDice = computed(() => {
+    const diceValues = this.game()?.dice.filter((d) => d.selectable && !d.scored).map((d) => d.value);
+    const sorted = diceValues?.sort((a, b) =>  a - b);
+    return sorted?.join('-') ?? '';
+  })
 }
